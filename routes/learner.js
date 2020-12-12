824ae6cd2db97285c3c6540240081853978dfc5f
Sam O <sam.ictdev@gmail.com>
2020-12-12 15:10:32 +0300

    test paper

--- routes/learner.js
+++ routes/learner.js
@@ -7,4 +7,10 @@ router.get("/", (req, res) => {
   res.send("Welcome to Learner Routes");
 });
 
+// Route to view modules.
+router.get("modules", (req, res) => {
+  res.render("modules");
+});
+
+
 module.exports = router;
